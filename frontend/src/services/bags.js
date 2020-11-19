9e3fdb3ac39bb5dc8f1f7491ef943a5fa10f7868
robertito1 <orazurobert@yahoo.com>
2020-11-19 21:27:04 +0000

    heroku test

--- frontend/src/services/bags.js
+++ frontend/src/services/bags.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/bags'
+const baseUrl = '/api/bags'
 
 
 const getBags = () => {
--- frontend/src/services/sales.js
+++ frontend/src/services/sales.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/sales'
+const baseUrl = '/api/sales'
 
 
 const getSales = () => {
